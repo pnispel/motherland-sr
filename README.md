2641a6da25d55222752bb97ea63f02d3ed7c75a1
pnispel <pnispel@gmail.com>
2015-01-22 09:22:09 -0600

    added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+See the live site at: https://motherland-sr.herokuapp.com/
--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url : 'mongodb://localhost/motherland'
+  // url : 'mongodb://localhost/motherland'
+  url: 'mongodb://pnispel:one2spin@ds031751.mongolab.com:31751/heroku_app33336633'
 }
